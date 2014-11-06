028b3296fc9f313add5ae1c282b5ac496bd22234
Kayla Jones <kaylalyne424@gmail.com>
2014-11-05 22:37:01 -0600

    rebuilt with grunt

--- public/views/mean/games.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  'use strict';
-  angular.module('portfolio')
-  .controller('GameCtrl', ['$scope', function($scope){
-    $scope.hello = 'Hello World';
-  }]);
-})();
--- public/views/work/work.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  'use strict';
-  angular.module('portfolio')
-  .controller('WorkCtrl', ['$scope', function($scope){
-    $scope.hello = 'Hello World';
-  }]);
-})();
