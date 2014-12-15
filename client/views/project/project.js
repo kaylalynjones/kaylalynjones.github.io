794a8c3c64a2927e535a6a61079c12c3c03b020d
Kayla Jones <kaylalyne424@gmail.com>
2014-12-14 21:54:37 -0600

    added project json info

--- client/views/project/project.js
+++ client/views/project/project.js
@@ -3,9 +3,11 @@
   angular.module('portfolio')
   .controller('ProjectCtrl', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http){
     $scope.project = $stateParams.name;
+    $scope.projects = [];
 
     $http.get('/public/data/projects.json').success(function(results){
-      console.log(results);
+      $scope.projects = results;
+      console.log($scope.projects);
     });
   }]);
 })();
