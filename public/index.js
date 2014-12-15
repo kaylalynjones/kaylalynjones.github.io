490a45adf5b1fadccea3c854d4609bfea366d8ba
Kayla Jones <kaylalyne424@gmail.com>
2014-12-14 18:16:16 -0600

    trying to switch to nui-router

--- public/index.js
+++ public/index.js
@@ -2,17 +2,20 @@ var smoothScroll, FastClick;
 
 (function(){
   'use strict';
-  angular.module('portfolio', ['ngRoute'])
-  .config(['$routeProvider', function($routeProvider){
-    $routeProvider
-    .when('/games',           {templateUrl:'public/views/games/games.html',       controller:'GameCtrl'})
-    .when('/mean',            {templateUrl:'public/views/mean/mean.html',         controller:'MeanCtrl'})
-    .when('/node',            {templateUrl:'public/views/node/node.html',         controller:'NodeCtrl'})
-    .when('/mobile',          {templateUrl:'public/views/mobile/mobile.html',     controller:'MobileCtrl'})
-    .when('/featured',        {templateUrl:'public/views/featured/featured.html', controller:'FeatureCtrl'})
-    .when('/hack',            {templateUrl:'public/views/hack/hack.html',         controller:'HackCtrl'})
-    .when('/work/hipsterbay', {templateUrl:'public/views/work/hipsterbay.html',   controller:'WorkCtrl'})
-    .otherwise({redirectTo:'/'});
+  angular.module('portfolio', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    .state('home',     {url:'/',        templateUrl:'public/views/home/home.html', controller:'HomeCtrl'})
+    .state('project',     {url:'',      abstract:true})
+    .state('home.games',     {url:'games',        templateUrl:'public/views/games/games.html', controller:'GameCtrl'})
+    .state('project.mean',      {url:'/mean',         templateUrl:'public/views/mean/mean.html',  controller:'MeanCtrl'})
+    .state('project.node', {url:'/node', templateUrl:'public/views/node/node.html',         controller:'NodeCtrl'})
+    .state('project.mobile', {url:'/mobile', templateUrl:'public/views/mobile/mobile.html',         controller:'MobileCtrl'})
+    .state('project.featured', {url:'/featured', templateUrl:'public/views/featured/featured.html',         controller:'FeatureCtrl'})
+    .state('project.hack', {url:'/hack', templateUrl:'public/views/hack/hack.html',         controller:'HackCtrl'});
+    //.state('work', {url:'/work', templateUrl: 'public/views/work/work.html', controller:'WorkCtrl'});
   }]);
   //smooth scrolling------------------------------------------------------------
   smoothScroll.init({
