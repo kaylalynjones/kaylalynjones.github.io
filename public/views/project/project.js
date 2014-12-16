9f0a9762bf4f8058ebbfe79718e0c57f3bc5c544
Kayla Jones <kaylalyne424@gmail.com>
2014-12-15 23:23:20 -0600

    detail pages for desktop complete

--- public/views/project/project.js
+++ public/views/project/project.js
@@ -2,12 +2,15 @@
   'use strict';
   angular.module('portfolio')
   .controller('ProjectCtrl', ['$scope', '$stateParams', '$http', function($scope, $stateParams, $http){
-    $scope.project = $stateParams.name;
+    var name = $stateParams.name;
+    $scope.project = {};
     $scope.projects = [];
 
     $http.get('/public/data/projects.json').success(function(results){
       $scope.projects = results;
-      console.log($scope.projects);
+      $scope.project = $scope.projects.filter(function(p){
+        return p.name.toLowerCase() === name.toLowerCase();
+      })[0];
     });
   }]);
 })();
